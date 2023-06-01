0aca68161d2bfe9b350e9dc3bee75edf327518dd
Makar Selivanov <makar.selivanov@gmail.com>
2023-06-01 15:16:28 +0300

    adding linked_list

---
+++ linked_list.h
@@ -0,0 +1,21 @@
+#ifndef HW_LINKED_LIST_H
+#define HW_LINKED_LIST_H
+
+typedef struct node_t {
+    struct node_t *next;
+    struct node_t *prev;
+    void *data;
+} node;
+
+typedef struct linked_list_t {
+    node *head;
+    size_t size;
+} linked_list;
+
+linked_list init_llist();
+void push_back(linked_list llist, void *data);
+void *get(linked_list llist, size_t index);
+void *remove(linked_list llist, size_t index);
+void free_list(linked_list llist);
+
+#endif //HW_LINKED_LIST_H
